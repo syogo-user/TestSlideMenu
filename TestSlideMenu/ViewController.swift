a11f847949b1843d60beeff542a3c66c192893d3
syogo-user <s.photo.trumpet.005@gmail.com>
2020-05-04 16:38:22 +0900

    Initial Commit

---
+++ TestSlideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSlideMenu
+//
+//  Created by 小野寺祥吾 on 2020/05/04.
+//  Copyright © 2020 syogo-user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
